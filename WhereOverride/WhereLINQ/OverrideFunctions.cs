895d6b7523f066d4e7f4930be91cdd6a609afd64
Fernando Solano <fesolano@ex2outcoding.local>
2019-04-15 17:14:28 -0600

    First Commit

---
+++ WhereOverride/WhereLINQ/OverrideFunctions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhereLINQ
+{
+    public static class OverrideFunctions
+    {
+        public static int Where()
+        {
+            return 0;
+        }
+    }
+}
---
+++ WhereOverride/WhereOverride/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhereOverride
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> array = new List<int>() {1, 2, 3, 5, 6, 7, 8, 9};
+            //string[] array = {"A", "B", "C", "D", "E"};
+            var query = array.Where(x => x > 5);
+            foreach (var number in query)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+
+    public static class LINQExtension
+    {
+        public static IEnumerable<T> Where<T>(this IEnumerable<T> source,
+                                                  Predicate<T> selector)
+        {
+            foreach (var collection in source)
+            {
+                if (selector(collection))
+                {
+                    yield return collection;
+                }
+            }
+        }
+    }
+}
